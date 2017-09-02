5684414c6c51b48996603ea642d23121dba9392f
errornosignal <errornosignal@users.noreply.github.com>
2017-09-02 13:56:32 -0500

    into commit

--- Apple_InClass01/main.swift
+++ Apple_InClass01/main.swift
@@ -1,8 +1,8 @@
 //
 //  main.swift
-//  Apple_InClass01
+//  InClass_Project01_08-31-17
 //
-//  Created by Reid Nolan on 9/2/17.
+//  Created by Reid Nolan on 8/31/17.
 //  Copyright © 2017 Reid Nolan. All rights reserved.
 //
 
@@ -10,3 +10,23 @@ import Foundation
 
 print("Hello, World!")
 
+let countOfWidgets = 13
+let todayIsThursday = true
+
+print("Hello World!")
+print("countOfWidgets = \(countOfWidgets)")
+print("todayIsThursday = \(todayIsThursday)")
+
+var intValue: Int? = 0
+var doubleValue: Double? = 0.0
+var inputString = ""
+
+print("Enter a whole number: ")
+inputString = readLine()!
+intValue = Int(inputString)
+print("intValue = \(String(describing: intValue))")
+
+print("Enter a floating-point value: ")
+inputString = readLine()!
+doubleValue = Double(inputString)
+print("doubleValue = \(String(describing: doubleValue))")
